2c50768e1478232a8a2c3e7c15eb3c306beb5a24
Jayden Rivett <jvyden420@gmail.com>
2018-07-27 20:16:01 -0400

    Initialize startup and stuff.

--- jvyterm-users/Plugin.cs
+++ jvyterm-users/Plugin.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using jvyterm;
+using System;
+
 
 namespace jvyterm_users
 {
     public class Plugin
     {
+        public static void init() {
+            login(true);
+            
+        }
+
+        public static void logout() { logout(); }
+
+        public static void login(bool isinit = false) {
+            logger.log("Login initialized. Prompting login.", logger.logType.silent);
+            //Console.Clear();
+            logger.log("", logger.logType.regular);
+            logger.log("Welcome to jvyterm. Please login.", logger.logType.regular);
+
+            Console.Write("u>");
+            string u = Console.ReadLine();
+            Console.Write("p>");
+            string p = Console.ReadLine();
+            
+
+        }
     }
 }
